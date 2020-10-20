236a263d307aecef12d395f3445dce67d1f5afa0
rikrdinii <rikrdinii@hotmail.com>
2020-10-19 21:43:30 -0500

    Lab casi terminado

--- your-code/main.py
+++ your-code/main.py
@@ -1,67 +1,75 @@
 #1. Import the NUMPY package under the name np.
-
+import numpy as np
 
 
 #2. Print the NUMPY version and the configuration.
-
+print(np.__version__)
 
 
 #3. Generate a 2x3x5 3-dimensional array with random values. Assign the array to variable "a"
 # Challenge: there are at least three easy ways that use numpy to generate random arrays. How many ways can you find?
-
+a = np.random.rand(2,3,5)
 
 
 #4. Print a.
-
+print(a)
 
 
 #5. Create a 5x2x3 3-dimensional array with all values equaling 1.
 #Assign the array to variable "b"
-
+b = np.random.rand(5,3,2)
 
 
 #6. Print b.
-
+print(b)
 
 
 #7. Do a and b have the same size? How do you prove that in Python code?
-
-
+a.shape == b.shape
+# Respuesta: no tienen el mismo tamaño. S
 
 
 #8. Are you able to add a and b? Why or why not?
 
-
+c = a+b
+# No se pueden sumar de manera directa ya que no tienen las mismas dimenciones
 
 #9. Transpose b so that it has the same structure of a (i.e. become a 2x3x5 array). Assign the transposed array to varialbe "c".
-
+c = b.T
 
 
 #10. Try to add a and c. Now it should work. Assign the sum to varialbe "d". But why does it work now?
-
-
+d = c+a
+# Ahora si funciona ya que tenemos dos matrices con las mismas dimenciones
 
 #11. Print a and d. Notice the difference and relation of the two array in terms of the values? Explain.
-
+print(a)
+print(d)
 
 
 
 #12. Multiply a and c. Assign the result to e.
-
+e = a*c
 
 
 #13. Does e equal to a? Why or why not?
+e == a
+e.shape == a.shape
 
-
+#Tienen el mismo tamaño, sin embargo, los datos contenidos en cada una de las matrices es completamente diferente
 
 
 #14. Identify the max, min, and mean values in d. Assign those values to variables "d_max", "d_min", and "d_mean"
+d_min = d.min()
+d_max = d.max()
+d_mean = d.mean()
 
+print(d_max,d_mean,d_min)
 
 
 
 #15. Now we want to label the values in d. First create an empty array "f" with the same shape (i.e. 2x3x5) as d using `np.empty`.
-
+f = np.empty
 
 
 
